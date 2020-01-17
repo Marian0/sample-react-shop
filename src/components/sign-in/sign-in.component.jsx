079288f1cba6c8f56b2c262184ee5680f34802ac
marian0 <marianosantafe@gmail.com>
2020-01-17 16:01:29 +0100

    Fix: prevent submit form when google sign in is clicked

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -65,7 +65,7 @@ const SignIn = () => {
               />
               <div className="buttons">
                 <CustomButton type='submit'>Sign in</CustomButton>
-                <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
+                <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
               </div>
             </form>
           </div>
