a7b1e71695219143c54dabbc6679ba9526fca3f3
marian0 <marianosantafe@gmail.com>
2020-01-23 16:54:56 +0100

    Added Error Boundaries

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import { createStructuredSelector } from 'reselect';
 import { selectCurrentUser } from 'redux/user/user.selector';
 import { GlobalStyle } from './global.styles';
 import Spinner from 'components/spinner/spinner.component';
+import ErrorBoundary from 'components/error-boundary/error-boundary.component';
 
 const HomePage = lazy(() => import('./pages/home/home.component'));
 const ShopPage = lazy(() => import('./pages/shop/shop.component'));
@@ -43,15 +44,17 @@ const App = ({ setCurrentUser, currentUser }) => {
       <GlobalStyle />
       <Header />
       <Switch>
-        <Suspense fallback={Spinner}>
-          <Route path="/" exact component={HomePage} />
-          <Route path="/shop" component={ShopPage} />
-          <Route exact path="/checkout" component={CheckoutPage} />
-          <Route exact path="/signin" render={() =>
-            currentUser ? <Redirect to='/shop' />
-              : <SignInPage />
-          } />
-        </Suspense>
+        <ErrorBoundary>
+          <Suspense fallback={Spinner}>
+            <Route path="/" exact component={HomePage} />
+            <Route path="/shop" component={ShopPage} />
+            <Route exact path="/checkout" component={CheckoutPage} />
+            <Route exact path="/signin" render={() =>
+              currentUser ? <Redirect to='/shop' />
+                : <SignInPage />
+            } />
+          </Suspense>
+        </ErrorBoundary>
       </Switch>
     </div>
   );
