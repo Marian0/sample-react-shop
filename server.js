588817ed90cdf5779c94527fec2caa720993bfd5
marian0 <marianosantafe@gmail.com>
2020-01-24 18:59:16 +0100

    Use compression gzip only on production

--- server.js
+++ server.js
@@ -14,12 +14,14 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT ||  5000;
 
-app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors()); 
 
 if (process.env.NODE_ENV === 'production') {
+  //Use gzip compression on production
+  app.use(compression());
+  
   //Force using ssl on production
   app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
